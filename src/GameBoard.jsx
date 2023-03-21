ed9f39925b3f3732d9e3e7d520614b4c191b02d7
mirzoyanani <animirzoyan.ait@gmail.com>
2023-03-21 22:45:49 +0400

    end

--- src/GameBoard.jsx
+++ src/GameBoard.jsx
@@ -1,33 +1,61 @@
 import React, { useContext, useState } from "react";
 import "./GameBoard.css";
-import Square from './Square'
+import Square from "./Square";
 
 const squares = Array(9).fill(null);
+
 const GameBoard = () => {
-const [value,setValue] = useState(true)
-
-function handleClick(index){
-    squares[index] = value?" X":'O'
-    setValue(!value)
-}
-function drawSquare(index){
+  const [value, setValue] = useState(true);
+  const winnersArray = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  function handleClick(index, e) {
+    squares[index] = value ? " X" : "O";
+    setValue(!value);
+    limitClick(e);
+  }
+  function handleWinner(squares) {
+    for (let i = 0; i < winnersArray.length; i++) {
+      const [a, b, c] = winnersArray[i];
+      if (
+        squares[a] &&
+        squares[a] === squares[b] &&
+        squares[a] === squares[c]
+      ) {
+        location.reload();
+      }
+    }
+  }
+  function drawSquare(index) {
     return (
-        <Square value = {squares[index]} 
-         onClick={()=>{handleClick(index)}}
-        />
-    )
-}
+      <Square
+        key={index}
+        value={squares[index]}
+        onClick={(e) => {
+          handleClick(index, e);
+          handleWinner(squares);
+        }}
+      />
+    );
+  }
+  function limitClick(e) {
+    e.target.disabled = true;
+  }
   return (
-
-    <div className="gameBoard">
-        {
-         squares.map((item,index)=>{
-            return(
-            drawSquare(index)
-            )
-         })
-        }
-    </div>
+    <>
+      <div className="gameBoard">
+        {squares.map((item, index) => {
+          return drawSquare(index);
+        })}
+      </div>
+    </>
   );
 };
 
